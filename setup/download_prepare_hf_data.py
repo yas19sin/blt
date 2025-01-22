11b7aaa2833a332ebcbae2836ce65f9883fc36b5
Yassine Ennaour <yas19sin@gmail.com>
2025-01-22 23:22:02 +0100

    Added "Lyte/fineweb-edu-2016-26-7M" as "fineweb_edu_7M" for smaller expirements

--- setup/download_prepare_hf_data.py
+++ setup/download_prepare_hf_data.py
@@ -91,6 +91,7 @@ def setup_terashuf(work_dir):
 def main(dataset, memory, data_dir, seed=42, nchunks=32, s3_profile: str | None = None):
     # Configuration
     repo_id = {
+        "fineweb_edu_7M": "Lyte/fineweb-edu-2016-26-7M",
         "fineweb_edu": "HuggingFaceFW/fineweb-edu",
         "fineweb_edu_10bt": "HuggingFaceFW/fineweb-edu",
         "dclm_baseline_1.0": "mlfoundations/dclm-baseline-1.0",
@@ -102,18 +103,21 @@ def main(dataset, memory, data_dir, seed=42, nchunks=32, s3_profile: str | None 
     work_dir = src_dir  # Directory of this Python file
     prefix = f"{dataset}.chunk."
     orig_extension = {
+        "fineweb_edu_7M": ".jsonl",
         "fineweb_edu": ".jsonl",
         "fineweb_edu_10bt": ".jsonl",
         "dclm_baseline_1.0": ".jsonl.zst",
         "dclm_baseline_1.0_10prct": ".jsonl.zst",
     }[dataset]
     cat_command = {
+        "fineweb_edu_7M": "cat",
         "fineweb_edu": "cat",
         "fineweb_edu_10bt": "cat",
         "dclm_baseline_1.0": "zstdcat",
         "dclm_baseline_1.0_10prct": "zstdcat",
     }[dataset]
     allow_patterns = {
+        "fineweb_edu_7M": None,
         "fineweb_edu": None,
         "fineweb_edu_10bt": "sample/10BT/*",
         "dclm_baseline_1.0": "*.jsonl.zst",
